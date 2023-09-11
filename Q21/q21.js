eb657533e3b0e69404b28849cde773e60a4a149b
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2023-09-11 11:23:29 +0500

    q21 added

---
+++ Q21/q21.js
@@ -0,0 +1,13 @@
+"use strict";
+// They think of something you could store in a TypeScript Object. 
+// Write a program that creates Objects containing these items.
+// Creating an array of country objects
+const countries = [
+    { name: "USA", population: 331002651, capital: "Washington, D.C." },
+    { name: "Canada", population: 37742154, capital: "Ottawa" },
+    { name: "Australia", population: 25499884, capital: "Canberra" },
+    { name: "Japan", population: 126476461, capital: "Tokyo" },
+    { name: "France", population: 65273511, capital: "Paris" }
+];
+// Printing the array of country objects
+console.log(countries);
