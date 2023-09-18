4617f64b16266f8b0c3b82a4edc68a844215f5bf
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2023-09-18 11:34:03 +0500

    Q45 added

---
+++ Q45/Q45.js
@@ -0,0 +1,15 @@
+"use strict";
+//Name	Muhammad Nouman Attique
+//Roll No	PIAIC206489
+//Batch	Batch 47
+function createCar(manufacturer, model, options = {}) {
+    const car = Object.assign({ manufacturer,
+        model }, options);
+    return car;
+}
+//Partial<Car> is a TypeScript utility type that makes all properties of the Car type optional.
+//This means you can provide some, all, or none of the properties when calling the function.
+// Call the function with required and optional information
+const myCar = createCar("Toyota", "Camry", { color: "Blue", year: 2022, features: ["GPS", "Leather Seats"] });
+// Print the created car object
+console.log(myCar);
